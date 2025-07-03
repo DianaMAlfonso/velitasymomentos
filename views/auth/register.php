c03d77f7bf3a594418b9844897e137fd4676d428
Diana <dianakendel13@gmail.com>
2025-07-02 21:07:18 -0500

    Readme actualizado

--- views/auth/register.php
+++ views/auth/register.php
@@ -1,7 +1,7 @@
 <?php require_once __DIR__ . '/../partials/header.php'; ?>
 
 <div class="row justify-content-center">
-    <div class="col-md-6">
+    <div class="col-md-6 mb-4">
         <h2>Registrarse</h2>
         <form action="index.php?action=register_user" method="POST">
             <div class="mb-3">
